233187f2b0c2665fea5406057cdfa126b7c02d00
georgiagrayland <>
2023-04-18 14:17:21 +0000

    h;

--- todo/test_views.py
+++ todo/test_views.py
@@ -4,6 +4,7 @@ from .models import Item
 # Create your tests here.
 class TestViews(TestCase):
 
+
     def test_get_todo_list(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
